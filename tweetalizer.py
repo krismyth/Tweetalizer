48a56d66023272887235ce7a403f8f5a6fe4ee7f
Kristi <kristi@kristis-macbook-pro.local>
2015-02-09 14:26:44 -0500

    functions refactoring

--- tweetalizer.py
+++ tweetalizer.py
@@ -8,49 +8,48 @@
 
 import sys
 
-def getTweetFromUser():
+def getTweetFromUser():  #if using python3, use input() instead of raw_input()
+  userInput = input("What tweet would you like to analyze? To quit, just enter 'q'. ")
+  if userInput == 'q' or userInput == 'Q':
+    exit()
+
+  return userInput
 
 def checkLength(tweet):
   tweetLength = len(tweet)
-  print("Tweet is " + str(tweetLength) + " characters long")
+  print("Tweet is %d characters long" % tweetLength)
 
   maxTweetLength = 140
-      if tweetLength > maxTweetLength:
+  if tweetLength > maxTweetLength:
+    print("The number of characters in this tweet is too damn high!\nThe limit is 140.")
 
-          print("The number of characters in this tweet is too damn high!\nThe limit is 140.")
+def checkHashtags(tweet):
+  hashCount = 0
+  for word in tweet.split(' '):
+    firstCharacter = word[0]
+    if firstCharacter == "#":
+      print("Tweet contains the hashtag: %s" % word)
+      hashCount += 1
+  print("The tweet has %d total #hashtags" % hashCount)
 
-def checkHashtags():
-
-
-def checkMentions():
-
+def checkMentions(tweet):
+  mentionCount = 0
+  for word in tweet.split(' '):
+    firstCharacter == word[0]
+    if firstCharacter == "@":
+      print("Tweet contains the mention: %s" % word)
+      mentionCount += 1
+    print("The tweet has %d total @mentions" % mentionCount)
 
 
 # primary function tweetalize:
 def tweetalize():
-    print("Welcome to the awesometastic tweetalizer! Start analyzing your tweets now!")
+  print("Welcome to the awesometastic tweetalizer! Start analyzing your tweets now!")
 
-    while True:  #if using python3, use input() instead of raw_input()
-      tweet = input("What tweet would you like to analyze? To quit, just enter 'q'. ")
-
-      if tweet == 'q' or tweet == 'Q':
-          exit()
-
-      checkLength(tweet)
-
-      keyword = 0
-      for hashtag in tweet.split(' '):
-          if hashtag[0] == "#":
-              print("Tweet contains the hashtag: " + hashtag)
-              keyword += 1
-      print("The tweet has " + str(keyword) + " total #hashtags")
-
-      mention = 0
-      for mentions in tweet.split(' '):
-          if mentions[0] == "@":
-              print("Tweet contains the mention: " + mentions)
-              mention += 1
-      print("The tweet has " + str(mention) + " total @mentions")
-
+  while True:
+    tweet = getTweetFromUser()
+    checkLength(tweet)
+    checkHashtags(tweet)
+    checkMentions(tweet)
 
 tweetalize()
